210c2bb2523745073d5a7363298f89b8242d4aee
jp-aub <109827323+jp-aub@users.noreply.github.com>
2023-01-23 15:11:12 -0500

    Established layout with basic constraints and stacks. Added live clock on label1.

--- A3-Clock/ViewController.swift
+++ A3-Clock/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    var timeAndDate = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        timeAndDate = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:#selector(self.second) , userInfo: nil, repeats: true)
     }
-
-
+    
+    @objc func second() {
+        dateLabel.text = DateFormatter.localizedString(from: Date(), dateStyle: .medium, timeStyle: .medium)
+    }
+    
 }
 
