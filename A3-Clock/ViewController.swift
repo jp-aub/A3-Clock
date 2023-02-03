c1a32d46811f3489622d726fb9179abdaf960321
jp-aub <109827323+jp-aub@users.noreply.github.com>
2023-02-03 08:47:32 -0500

    Added documentation/comments and adjusted constraints and spacing for different device sizes.

--- A3-Clock/ViewController.swift
+++ A3-Clock/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -14,13 +15,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var timerBtn: UIButton!
     @IBOutlet weak var timeRemaining: UILabel!
     
-    var timeAndDate = Timer()
-    var timeLeft : Int?
-    var countdownTime = Timer()
+    var timeAndDate = Timer() // Timer for current time and date
+    var timeLeft : Int? // Will hold the remaining countdown time left
+    var countdownTime = Timer() // Timer for countdown
+    var countdownBtnStatus = 0; // 0 = countdown not started
+    var musicTimer = Timer() // Timer for music that plays upon countdown reaching 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        timeRemaining.text = ""
         
         // Set up current time. Must be done before timer to ensure no delay upon start up.
         timeNow()
@@ -33,6 +37,7 @@ class ViewController: UIViewController {
         timeAndDate = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:#selector(self.timeNow) , userInfo: nil, repeats: true)
     }
     
+    // Provides functionality for the live clock.
     @objc func timeNow() {
         let date = Date()
         let dateFormatting = DateFormatter()
@@ -41,23 +46,23 @@ class ViewController: UIViewController {
         checkTime()
     }
     
+    // Establishes the current time (hour) and adjusts the user interface (light or dark mode) based on AM or PM.
     func checkTime() {
         let time = Date()
         let hour = DateFormatter()
         hour.dateFormat = "HH"
-        let currentHour = hour.string(from: time)
+        let currentHour = Int(hour.string(from: time))
         
         // Perform the time comparison. 12 represents 12:00 or when the clock changes to PM
-        let comparison = currentHour.compare("12")
-        
         // If the current time is > 0 (current time is after 12:00), change background/color theme
-        if comparison.rawValue > 0 {
+        if currentHour! >= 12 {
             darkMode()
         } else {
             lightMode()
         }
     }
     
+    // Changes user interface to dark mode.
     func darkMode() {
         view.backgroundColor = UIColor.black
         dateLabel.textColor = UIColor.white
@@ -69,6 +74,7 @@ class ViewController: UIViewController {
         timeRemaining.textColor = UIColor.white
     }
     
+    // Changes user interface to light mode.
     func lightMode() {
         view.backgroundColor = UIColor.white
         dateLabel.textColor = UIColor.black
@@ -90,7 +96,18 @@ class ViewController: UIViewController {
     
     @IBAction func startTimer(_ sender: UIButton) {
         countdownTime.invalidate()
-        countdownTime = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(countdown), userInfo: nil, repeats: true)
+        // Check if timer is already running.
+        // If not, start timer and change button text.
+        if countdownBtnStatus == 0 {
+            countdownTime = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(countdown), userInfo: nil, repeats: true)
+        } else if countdownBtnStatus == 1 {
+            AudioServicesDisposeSystemSoundID(1151)
+            musicTimer.invalidate()
+            countdownBtnStatus = 0;
+            timerBtn.setTitle("Start Timer", for: .normal)
+            getCountdown()
+        }
+        
     }
     
     @objc func countdown() {
@@ -107,8 +124,29 @@ class ViewController: UIViewController {
         // Set label text with formatted remaining time
         timeRemaining.text = "Time Remaining: \(formattedTime)"
         
+        // If the countdown timer reaches 0, change button text and start the music.
+        if timeLeft! <= 0 {
+            countdownTime.invalidate()
+            timerBtn.setTitle("Stop Music", for: .normal)
+            countdownBtnStatus = 1;
+            startMusicTimer()
+        }
+        
         // Decrement timer by one second
         timeLeft! -= 1
+    }
+    
+    func startMusicTimer() {
+        // Play sound once before timer starts to avoid initial delay
+        AudioServicesPlaySystemSound(1151)
+        
+        // Begin timer that plays the system sound every two seconds until cancelled
+        countdownTime = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(music), userInfo: nil, repeats: true)
+    }
+    
+    // Called by the timer function to play music/system sound
+    @objc func music() {
+        AudioServicesPlaySystemSound(1151)
     }
 }
 
